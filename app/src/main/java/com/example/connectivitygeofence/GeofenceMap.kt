26e3d2e59a0ce5019ac5561e17fc97f4d7c3d42a
sebastian reyes <sr0reyes0@gmail.com>
2020-08-11 15:04:17 -0500

    first commit

---
+++ app/src/main/java/com/example/connectivitygeofence/GeofenceMap.kt
@@ -0,0 +1,136 @@
+package com.example.connectivitygeofence
+
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.renderscript.Sampler
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
+import java.util.jar.Manifest
+
+class GeofenceMap : AppCompatActivity(), OnMapReadyCallback {
+
+    companion object{
+        const val TAG = "GeofenceMap"
+        const val DEFAULT_ZOOM = 15f
+    }
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    //Widgets
+    private lateinit var editText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geofence_map)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        editText= findViewById(R.id.editText)
+        editText.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                //execute method for searching
+                geoLocate()
+            }
+            false
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        getDevicePosition()
+        zoomAtDevicePosition()
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun getDevicePosition(){
+        if (ContextCompat.checkSelfPermission(applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            mMap.isMyLocationEnabled = true
+            changeButtonPosition()
+        }
+        return
+    }
+
+    private fun zoomAtDevicePosition(){
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if(location != null){
+                val latLng = LatLng(location.latitude, location.longitude)
+                moveCamera(latLng, DEFAULT_ZOOM)
+            }
+        }
+    }
+
+    private fun moveCamera(latLng: LatLng, zoom: Float){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom))
+    }
+
+    private fun geoLocate(){
+        val searchString = editText.text.toString()
+        Log.d(TAG, searchString)
+        val geoCoder = Geocoder(this)
+        var list = listOf<Address>()
+
+        try {
+            list = geoCoder.getFromLocationName(searchString,1)
+        }catch(e: IOException){
+            Log.e(TAG,"geolocate IOException" + e.message)
+        }
+
+        if(list.isNotEmpty()){
+            val address = list.get(0)
+            Log.d(TAG, "geoLocate found location: $address")
+            val latLng = LatLng(address.latitude, address.longitude)
+            moveCamera(latLng, DEFAULT_ZOOM)
+        }
+    }
+
+    private fun changeButtonPosition(){
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map)
+        val locationButton= (mapFragment?.view?.findViewById<View>(Integer.parseInt("1"))?.parent as View).findViewById<View>(Integer.parseInt("2"))
+        val rlp=locationButton.layoutParams as (RelativeLayout.LayoutParams)
+        // position on right bottom
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP,RelativeLayout.TRUE)
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0)
+        rlp.setMargins(0,160,60,0);
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/connectivitygeofence/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.example.connectivitygeofence
+
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    companion object{
+        const val TAG = "MainActivity"
+        const val FINE_LOCATION_REQUEST_CODE = 1001
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+        getPermissions()
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, GeofenceMap::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun getPermissions(){
+        when{
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
+                // Permissions are granted
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                //Show the dialog explaining why you need the permissions
+                showDialog()
+            }
+            else -> {
+                // Just Ask for the permissions
+                requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    FINE_LOCATION_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            FINE_LOCATION_REQUEST_CODE ->{
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //Permission is granted.
+
+                }else{
+                    // User did´t grant the permissions so close the app
+                    finish()
+                }
+            }
+        }
+
+    }
+
+    private fun showDialog(){
+        val builder: AlertDialog.Builder? = this.let{
+            AlertDialog.Builder(it)
+        }
+
+        builder?.setMessage(R.string.permissions_message)
+        builder?.setTitle(R.string.app_name)
+
+        builder?.setPositiveButton(R.string.ok, DialogInterface.OnClickListener { dialog, id ->
+            //ask for permissions
+            requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                FINE_LOCATION_REQUEST_CODE)
+        })
+
+        val dialog: AlertDialog? = builder?.create()
+
+        dialog?.show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
